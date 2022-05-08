0ac0b6c33f8e4615f40608fe3695e3760dce6471
Alexantoun <oz.antoun@gmail.com>
2022-05-08 14:27:39 -0600

    Hello

--- server.py
+++ server.py
@@ -4,60 +4,64 @@ import csv
 
 def parse_input(s1: str, s2: str, s3: str) -> list:
     l = list()
-
-    l.append(datetime.datetime.now())
-
+    x = datetime.datetime.now()
+    l.append(x.strftime('%X'))
+    #ButtonStatus
     if(s1[0] == 'b'):
         l.append(s1[1:])
-    if(s2[0] == 'b'):
+    elif(s2[0] == 'b'):
+        l.append(s2[1:])
+    elif(s3[0] == 'b'):
+        l.append(s3[1:])
+    #Temperature
+    if(s1[0] == 'c'):   
         l.append(s1[1:])
-    if(s3[0] == 'b'):
-        l.append(s1[1:])
-
-    if(s1[0] == 'c'):  
-        l.append(s1[1:])
-    if(s2[0] == 'c'):
-        l.append(s1[1:])
-    if(s3[0] == 'c'):
-        l.append(s1[1:])
-
+    elif(s2[0] == 'c'):
+        l.append(s2[1:])
+    elif(s3[0] == 'c'):
+        l.append(s3[1:])
+    #Humidity 
     if(s1[0] == 'h'):
         l.append(s1[1:])
-    if(s2[0] == 'h'):
-        l.append(s1[1:])
-    if(s3[0] == 'h'):
-        l.append(s1[1:])
-    
-    return l
-
-
-def write_to_csv(l :list, writer):
-    print(f'current list: {l}')
-    if(len(l)!=3):
-        return
-    if(l[0]=='False' or l[0]=='True'):
-        if(l[-1]=='False' or l[-1]=='True'):
-            return
-
-        writer.writerow(l)
+    elif(s2[0] == 'h'):
+        l.append(s2[1:])
+    elif(s3[0] == 'h'):
+        l.append(s3[1:])
+    if (len(l)==4):
+        return l
+    else:
+        print("Error in parse_input")
         print(l)
-        l.clear()
-        
-        return
+        return []
 
-    elif(l[-1]=='False' or l[-1]=='True'):
-        new_l = []
-        for x in l:
-            if(x=='False' or x=='True'):
-                new_l = [x] + new_l
-            else:
-                new_l = new_l + [x]
 
-        writer.writerow(l)
-        print(l)
-        l = []
+# def write_to_csv(l :list, writer):
+#     print(f'current list: {l}')
+#     if(len(l)!=3):
+#         return
+#     if(l[0]=='False' or l[0]=='True'):
+#         if(l[-1]=='False' or l[-1]=='True'):
+#             return
+
+#         writer.writerow(l)
+#         print(l)
+#         l.clear()
         
-        return
+#         return
+
+#     elif(l[-1]=='False' or l[-1]=='True'):
+#         new_l = []
+#         for x in l:
+#             if(x=='False' or x=='True'):
+#                 new_l = [x] + new_l
+#             else:
+#                 new_l = new_l + [x]
+
+#         writer.writerow(l)
+#         print(l)
+#         l = []
+        
+#         return
         
 
 
@@ -81,18 +85,20 @@ def main():
                 if recieved=="close\r\n":
                     break   #Go back to wait-for-connection state
                 arduinoList.append(recieved)
+                
                 x+=1
                 if x==3:
-                    x = x%3
-                    # writer.writerow(arduinoList)
-                    # print(arduinoList)
-                    # arduinoList.clear() 
-                    write_to_csv(arduinoList, writer)
+                    arduinoList = parse_input(*arduinoList)
+                    if(len(arduinoList) == 4):
+                        x = 0
+                        writer.writerow(arduinoList)
+                    print(arduinoList)
+                    arduinoList.clear() 
 
 
 header = ['TimeStamp', 'Button', 'Temp-C', 'Humidity-%']
 # file = open('Arduino Data.csv', 'w'a
-with open('arduinoData.csv', 'w') as f:    
+with open('arduinoData.csv', 'a') as f:    
     writer = csv.writer(f)
     writer.writerow(header)
     main()      
